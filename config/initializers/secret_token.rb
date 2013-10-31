13e6b196fba327d76eb044fe151449bd3732c7d4
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-10-30 19:21:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaltyenergyGithubIo::Application.config.secret_token = '1f1d473fc89d3bbd8e9e214d54ff783c9b3d79a3494e53209c6af8d91547a2f5439304515e4283980e1e35c19698ed1a39c7d8c3d97f3d4d9d7d8d877c2f6b1a'
